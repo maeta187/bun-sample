d412d0f7d35cb41d849f24d033df0b7698da8152
maeta187 <nexus18789@gmail.com>
2024-02-24 15:29:02 +0900

    ✨ : ルーティングの実装

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 const server = Bun.serve({
   port: 3000,
   fetch(request) {
-    return new Response('Welcome to Bun!')
+    const url = new URL(request.url)
+    if (url.pathname === '/') return new Response('Home page!')
+    if (url.pathname === '/list') return new Response('List page!')
+    if (url.pathname === '/blog') throw new Error('woops!')
+    return new Response('404!')
+  },
+  error(error) {
+    return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
   }
 })
 
-console.log(`Listening on localhost:${server.port}`)
+console.log(`Listening on localhost: ${server.port}`)
